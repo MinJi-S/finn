0a6c9d8fa9ee0e86edd276cea2815e8ce6c2a133
yslim <yslim93@naver.com>
2018-11-05 14:10:05 +0900

    sessionFactory 부분 수정

--- src/main/java/net/herit/FinnApplication.java
+++ src/main/java/net/herit/FinnApplication.java
@@ -1,16 +1,8 @@
 package net.herit;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
-
-import javax.sql.DataSource;
 
 /**
  *
@@ -19,7 +11,6 @@ import javax.sql.DataSource;
  * @author yslim
  */
 @SpringBootApplication
-@EnableAutoConfiguration
 @Slf4j
 public class FinnApplication {
 
@@ -27,20 +18,5 @@ public class FinnApplication {
 		SpringApplication.run(FinnApplication.class, args);
 
 		log.info("************Finn And Jake!!!*****************");
-	}
-
-	/**
-	 * SqlSessionFactory 설정
-	 */
-	@Bean
-	public SqlSessionFactory sqlSessionFactory(DataSource dataSource)throws Exception{
-		SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
-		sessionFactory.setDataSource(dataSource);
-
-		Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:mappers/*Mapper.xml");
-
-		sessionFactory.setMapperLocations(res);
-
-		return sessionFactory.getObject();
 	}
 }
--- src/main/java/net/herit/config/ApplicationProperties.java
+++ src/main/java/net/herit/config/ApplicationProperties.java
@@ -2,10 +2,19 @@ package net.herit.config;
 
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
-@Configuration
+import javax.sql.DataSource;
+
+@Configuration	// 어플리케이션 컨텍스트 또는 빈 팩토리라고 알려주는 것
+@MapperScan(basePackages = {"net.herit.mapper"})
 @Data
 @Slf4j
 public class ApplicationProperties {
@@ -15,5 +24,20 @@ public class ApplicationProperties {
 	
 	public ApplicationProperties() {
 		log.debug("\n======ApplicationProperties contructor.=======\n");
+	}
+
+	/**
+	 * SqlSessionFactory 설정 빈 등록
+	 */
+	@Bean
+	public SqlSessionFactory sqlSessionFactory(DataSource dataSource)throws Exception{
+		SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+		sessionFactory.setDataSource(dataSource);
+
+		Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:mappers/*Mapper.xml");
+
+		sessionFactory.setMapperLocations(res);
+
+		return sessionFactory.getObject();
 	}
 }
